8ec9bf353de8f645582631f7e6f79dbd2137aee5
Pasan <pasanblibrary@gmail.com>
2023-12-23 22:30:40 +0530

    Project created

---
+++ models/Book.js
@@ -0,0 +1,18 @@
+// models/Book.js
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    genre: { type: [String], default: [] },
+    isAvailable: { type: Boolean, default: false },
+    image: { type: String, required: true },
+    description: { type: String, required: true },
+  },
+  { collection: 'books' },
+);
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
